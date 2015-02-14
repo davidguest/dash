f517a21281f968b6973869e275fbf5fdf3c44e4a
davidguest <d.guest@sussex.ac.uk>
2015-02-14 15:53:52 +0000

    adding zepto and base functions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
-var test = "hello";
+//document.addEventListener("deviceready", wakeUp, false);
+
+var url="http://www.davidguest.me.uk/hello/data";
+var mainContent = "";
+
+function get(postfields, cback) {
+    var xhr = new XMLHttpRequest;
+    xhr.onload = function() {
+        cback(xhr.responseText);
+    }
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded; charset=utf-8");
+    xhr.send(postfields);
+}
+
+function getData() {
+
+    var postfields = "";
+    get( postfields, prepData);
+
+}
+
+function prepData(json) {
+
+    var basicData = JSON.parse(json);
+    var trafficLights = basicData.trafficLights;
+    mainContent = "<p>Email status is "+trafficLights.email + "<br />Printing status is "+trafficLights.printing+"</p>";
+    updateMain();
+    
+
+}
+
+function updateMain() {
+
+    $("#main").html(mainContent);
+
+}
+
+function init() {
+    
+    mainContent = "<p>Ready...fetching data...</p>";
+    updateMain();
+    getData();
+
+}
+
+$(document).ready( function() {
+    
+    init();
+
+});
